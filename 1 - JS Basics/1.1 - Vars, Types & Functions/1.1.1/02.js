03a2aac9efd2193aa623d07a77cce9ec982cb277
scorberan <116673857+scorberan@users.noreply.github.com>
2023-02-09 07:32:47 +0100

    Update 02.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
@@ -4,3 +4,15 @@ const a = 4;
 a = 5;
 
 console.log(a);
+// para que te salga solo 4 en la consola 
+
+const a = 4;
+
+console.log(a);
+
+// Y si quisiera que 5 también se enseñase 
+const a = 4;
+const b = 5;
+
+console.log(a);
+console.log(b);
